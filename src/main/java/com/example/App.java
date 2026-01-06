707b61d1f10b7ad5fd429b78af5453397b33e504
Puneet Kumar <164636741+The-Puneet-Kumar@users.noreply.github.com>
2026-01-06 22:21:37 +0530

    Change output message in App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,5 @@
-package com.example;
-
 public class App {
     public static void main(String[] args) {
-        System.out.println("Jenkins CI/CD Pipeline is working 🚀");
+        System.out.println("Hello Jenkins CI/CD Pipeline 🚀");
     }
 }
